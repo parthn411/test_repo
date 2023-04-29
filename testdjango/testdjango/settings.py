794e028223030217e0e521e084dc91b07ef975f2
parthn411 <elloraparth@gmail.com>
2023-04-29 18:03:04 +0530

    CompareCommit

--- testdjango/testdjango/settings.py
+++ testdjango/testdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_d#32ni8ob4u^+p3gm&lge!rzyy@2mlr!($ls8(c9$13@*)1_+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
